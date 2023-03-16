2a2e7ac79d6fd12a338c3f82f40f67621e3c1fce
Brian Benz <bbenz@microsoft.com>
2023-03-16 01:27:45 -0700

    Remove the build on the runner

--- vmbuildscript.sh
+++ vmbuildscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+sudo apt-get install msopenjdk-11
 sudo apt install nodejs npm
 sudo npm install
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install msopenjdk-11
 sudo ./mvnw -Pprod clean verify

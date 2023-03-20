50cb761b416af058940a39c1490bed6591eac9a4
Brian Benz <bbenz@microsoft.com>
2023-03-20 10:38:12 -0700

    add copy to appbuildscript

--- appbuildscript.sh
+++ appbuildscript.sh
@@ -2,4 +2,6 @@
 
 echo building app distribution - mvnw -Pprod clean verify
 ./mvnw -Pprod clean verify
+echo copying vmbuildscript.sh target
+cp vmbuildscript.sh target
 echo app distribution build complete - ls -l target

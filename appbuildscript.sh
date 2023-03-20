a06b48dcb8401cf176c3722423d414b8320f34fa
Sandra Ahlgrimm <sandra.kriemann@gmail.com>
2023-03-20 13:11:10 +0100

    Update appbuildscript.sh
    skip tests for demo purpose

--- appbuildscript.sh
+++ appbuildscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo building app distribution - mvnw -Pprod clean verify
-./mvnw -Pprod clean verify
+./mvnw -Pprod -DskipTests clean verify
 echo app distribution build complete - ls -l target
